cd55070c00fb101c2a29def8f8aaf3ac5f3eaa2f
vinayredkar <vinayredkar43@gmail.com>
2022-08-27 15:41:59 +0530

    login Button

--- 1.py
+++ 1.py
@@ -13,7 +13,10 @@ def First():
 @app.route('/Login')
 def Login():  
    return render_template('Login.html')
-   
+
+@app.route('/About_Us')
+def About_Us():
+   return render_template('About_Us.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_debugger=False, use_reloader=False)
